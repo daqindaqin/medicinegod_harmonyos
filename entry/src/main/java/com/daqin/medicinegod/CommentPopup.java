d33301546115743583048a2e541cd438fbab9ec7
daqin <wfgmqhx@gmail.com>
2021-12-17 00:00:49 +0800

    完善了otc的疑问弹窗

--- entry/src/main/java/com/daqin/medicinegod/CommentPopup.java
+++ entry/src/main/java/com/daqin/medicinegod/CommentPopup.java
@@ -27,54 +27,6 @@ public class CommentPopup extends BottomPopupView {
     @Override
     protected void onCreate() {
         super.onCreate();
-        /*
-        findComponentById(ResourceTable.Id_tv_temp).setClickedListener(new ClickedListener() {
-            @Override
-            public void onClick(Component component) {
-                // 弹出新的弹窗用来输入
-                final CustomEditTextBottomPopup textBottomPopup = new CustomEditTextBottomPopup(getContext());
-                new XPopup.Builder(getContext())
-                        .autoOpenSoftInput(true)
-                        .setComponent(ZhihuCommentPopup.this) // 用于获取页面根容器，监听页面高度变化，解决输入法盖住弹窗的问题
-                        .setPopupCallback(new SimpleCallback() {
-                            @Override
-                            public void onShow(BasePopupView popupView) {
-                            }
-
-                            @Override
-                            public void onDismiss(BasePopupView popupView) {
-                                String comment = textBottomPopup.getComment();
-                                if (!comment.isEmpty()) {
-                                    data.add(0, comment);
-                                    commonAdapter.notifyDataChanged();
-                                }
-                            }
-                        })
-                        .asCustom(textBottomPopup)
-                        .show();
-            }
-        });
-
-        listContainer.setItemClickedListener(new ListContainer.ItemClickedListener() {
-            @Override
-            public void onItemClicked(ListContainer listContainer, Component component, int position, long id) {
-                // 可以等消失动画执行完毕再开启新界面
-                dismissWith(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent secondIntent = new Intent();
-                        Operation operation = new Intent.OperationBuilder()
-                                .withBundleName(getContext().getBundleName())
-                                .withAbilityName(DemoAbility.class.getName())
-                                .build();
-                        secondIntent.setOperation(operation);
-                        getContext().startAbility(secondIntent, 0);
-                    }
-                });
-            }
-        });
-        listContainer.setItemProvider(commonAdapter);
-        */
     }
 
     @Override
